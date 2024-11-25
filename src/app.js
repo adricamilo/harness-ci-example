b8ed9ff24767dec3a64547c34da0a62452997c66
adricamilo <adrian.vasconez@gmail.com>
2024-11-25 15:17:48 -0500

    Edit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (_req, res) =>  {
-  res.status(200).send("Hello World!")
+  res.status(200).send("Hello World 2!")
 })
 module.exports = app;
