b1327a977d0870083c5e30c0039445a889686a63
adricamilo <adrian.vasconez@gmail.com>
2024-11-25 15:19:05 -0500

    Edit2

--- src/app.test.js
+++ src/app.test.js
@@ -6,6 +6,6 @@ describe("/hello endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/hello")
         expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello World!");
+        expect(response.text).toBe("Hello World 2!");
     })
 })
